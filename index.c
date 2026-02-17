03c6b5f591f8dcdc97a3f10ef446d008cf638be7
GopikaNairAS <gopikanairas06@gmail.com>
2026-02-17 19:49:27 +0530

    Implement function to find single number in array

---
+++ index.c
@@ -0,0 +1,17 @@
+int singleNumber(int* nums, int numsSize) {
+    int i,j,s=0,c=0;
+    for(i=0;i<32;i++)
+    {
+        c=0;
+        for(j=0;j<numsSize;j++)
+        {
+            if(nums[j]&((uint32_t)1<<i))
+            {
+                c++;
+            }
+        }
+        c=c%3;
+        s=s|((uint32_t)c<<i);
+    }
+    return s;
+}
